dcd227f28ed00d56c613904f167125c3d32c188c
david.cavalcanti <david.cavalcanti@i.systems.com.br>
2023-09-20 00:11:10 -0300

    Create first get requisition

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connectDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
 
 const connection = await connectDatabase();
 
@@ -7,14 +8,25 @@ connection.on("Error", (erro) => {
     console.error("Erro de conexão", erro);
 });
 
-connection.once("open", ()=> {
+connection.once("open", () => {
     console.log("Conexão com o banco de dados feita com sucesso!");
 });
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.status(200).send("API Development");
+})
+
+app.get('/livros', async (req, res) => {
+    try {
+        const livrosResultado = await livro.find();
+        res.status(200).json(livrosResultado)
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 export default app;
