9fa9fbcd59b437832dfab448afa00be51fa68c59
David <david_cavalcanti@outlook.com>
2023-09-23 11:07:22 -0300

    Update object type

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -5,7 +5,7 @@ const livroSchema = new mongoose.Schema({
     title: { type: mongoose.Schema.Types.String, required: true },
     autor: { type: mongoose.Schema.Types.String, required: true },
     editora: { type: mongoose.Schema.Types.String, required: true },
-    numeroPagina: { type: Number }
+    numeroPagina: { type: mongoose.Schema.Types.Number }
 }, { versionKey: false });
 
 const livro = mongoose.model('livros', livroSchema);
