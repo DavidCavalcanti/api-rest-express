b0a88d302aa204b7955105183333e578f6e62ec2
David <david_cavalcanti@outlook.com>
2023-09-24 15:56:41 -0300

    Create routes for author

---
+++ src/routes/autorRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import AutorController from "../controller/autorController.js";
+
+const autorRoutes = express.Router();
+
+autorRoutes
+    .get("/autor", AutorController.listarAutores)
+    .get("/autor/:id", AutorController.listaAutorId)
+    .post("/autor", AutorController.cadastrarAutor)
+    .put("/autor/:id", AutorController.atualizarAutor)
+    .delete("/autor/:id", AutorController.deletarAutor)
+
+export default autorRoutes;
