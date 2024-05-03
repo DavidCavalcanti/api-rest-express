a657e1a89571fe78d5e6c47607c89a5d4e8967b6
David <david_cavalcanti@outlook.com>
2024-05-03 12:30:39 -0300

    feature/pagination - add paging middleware

--- src/controller/livroController.js
+++ src/controller/livroController.js
@@ -1,29 +1,15 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js";
-import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 import { autor, livro } from "../models/index.js";
 
 class LivroController {
 
   static async listarLivros(req, res, next) {
     try {
-      let { limite = 5, pagina = 1, ordenacao = "_id:-1"} = req.query;
+      const buscaLivro = livro.find();
 
-      limite = parseInt(limite);
-      pagina = parseInt(pagina);
-      const [campoOrdenacao, ordem] = ordenacao.split(":");
+      req.resultado = buscaLivro;
 
-      if (limite > 0 && pagina > 0) {
-        const livrosResultado = await livro.find()
-          .sort({ [campoOrdenacao]: ordem })
-          .skip((pagina - 1) * limite)
-          .limit(limite)
-          .populate("autor")
-          .exec();
-        res.status(200).json(livrosResultado);
-      } else {
-        next(new RequisicaoIncorreta());
-      }
-
+      next();
     } catch (erro) {
       next(erro);
     }
@@ -86,11 +72,13 @@ class LivroController {
       const busca = await processaBusca(req.query);
 
       if (busca !== null) {
-        const livrosResultado = await livro
+        const livrosResultado = livro
           .find(busca)
           .populate("autor");
+      
+        req.resultado = livrosResultado;
 
-        res.status(200).json(livrosResultado);
+        next();
       } else {
         res.status(200).json({ message: "Nenhum autor foi encontrado." });
       }
@@ -113,7 +101,7 @@ async function processaBusca(parametros) {
   if (minPaginas) busca.numeroPaginas.$gte = minPaginas;
   if (maxPaginas) busca.numeroPaginas.$lte = maxPaginas;
 
-  if (nomeAutor) {
+  if (nomeAutor) {        
     const autorResultado = await autor.findOne({ nome: nomeAutor });
 
     if (autorResultado !== null) {
--- src/routes/livroRoutes.js
+++ src/routes/livroRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import LivroController from "../controller/livroController.js";
+import paginar from "../middlewares/paginar.js";
 
 const livroRoutes = express.Router();
 
 livroRoutes
-  .get("/livros", LivroController.listarLivros)
-  .get("/livros/busca", LivroController.listarLivroPorFiltro)
+  .get("/livros", LivroController.listarLivros, paginar)
+  .get("/livros/busca", LivroController.listarLivroPorFiltro, paginar)
   .get("/livros/:id", LivroController.listarLivroID)
   .post("/livros", LivroController.cadastrarLivro)
   .put("/livros/:id", LivroController.atualizarLivro)
